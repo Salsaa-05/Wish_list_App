f3528f345e75908c7259cfd618644f1a71321cd5
Administrator <saadhamouda95@gmail.com>
2020-05-01 00:48:34 +0200

    add courses app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CourseList from './Component/CourseList'
+import CourseForm from './Component/CourseForm'
+class App extends Component {
+  state = {
+    courses: [
+      { name: "HTML" },
+      { name: "CSS" },
+      { name: "JavaScript" },
+    ],
+    current: ''
+  }
+  //Update while writing in the input
+  updateCourse = (e) => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    this.setState({
+      current: e.target.value
+    })
+  }
+  //adding
+  addCourse = (e) => {
+    e.preventDefault();
+    let current = this.state.current;
+    let courses = this.state.courses;
+    courses.push({ name: current })
+    this.setState({
+      courses: courses,
+      current : ''
+    })
+
+  }
+  render() {
+    const { courses } = this.state;
+    const courseList = courses.map((course, index) => {
+      return <CourseList details={course} key={index} update={this.handleChange} />
+    })
+    return (
+      <section className="App">
+        <h2>Add courses</h2>
+        <CourseForm updateCourse={this.updateCourse} addCourse={this.addCourse} currentvalue ={this.state.current} />
+        <ul>{courseList}</ul>
+      </section>
+    );
+  }
+
 }
+
 
 export default App;
---
+++ src/Component/CourseList.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class CourseList extends Component{
+render(){
+
+  return(
+   <React.Fragment>
+       <li>
+           {this.props.details.name}
+       </li>
+   </React.Fragment>
+  );
+}
+
+}
+
+
+export default CourseList;
+ 
